cc849f52a62f87f28ecb3cda4e162aeed46ec57a
Samuel Wölfl <samuelwoelfl@gmx.de>
2021-06-06 21:06:39 +0200

    adjusted further logo links

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -62,7 +62,7 @@ function switchTheme(theme) {
     } catch (err) {}
     try {
       const docs_logo = document.querySelector(".docs-logo");
-      docs_logo.setAttribute("src", "./img/docs/logo.png");
+      docs_logo.setAttribute("src", "../img/docs/logo.png");
     } catch (err) {}
 
 
@@ -94,7 +94,7 @@ function switchTheme(theme) {
     } catch (err) {}
     try {
       const docs_logo = document.querySelector(".docs-logo");
-      docs_logo.setAttribute("src", "./img/docs/logo_white.png");
+      docs_logo.setAttribute("src", "../img/docs/logo_white.png");
     } catch (err) {}
 
   }
